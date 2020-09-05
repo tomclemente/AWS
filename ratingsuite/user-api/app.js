fd67568de91cec239f184231be595cb3af9ae309
tomclemente <tomangelo@gmail.com>
2020-09-06 02:58:27 +0800

    Added more DEL logic

--- ratingsuite/user-api/app.js
+++ ratingsuite/user-api/app.js
@@ -43,13 +43,19 @@ exports.handler = async (event, context) => {
                 case 'POST': // Read user details from the auth token
                             // Create an entry in usermaster
                     insertUserMaser(params,username).then(function() {
-                        var emailParam = generateWelcomeParam(username);
-                        sendEmail(emailParam).then(resolve,reject);
-                    });
+
+                        sql = "SELECT * FROM UserPool where userid = '" + username + "'";
+                        executeQuery(sql).then(function(data) {
+                            insertNotification(username, data.upid, params);
+
+                            var emailParam = generateWelcomeParam(username);
+                            sendEmail(emailParam).then(resolve,reject);
+                        })    
+                    },reject);
                 break;
                     
                 case 'PUT': // Update cognito record and usermaster table        
-                    updateUserAttribute('email', params.email, username, process.env.COGNITO_POOLID).then(function() {
+                    updateUserAttribute(params.attributes, username, process.env.COGNITO_POOLID).then(function() {
                         sql = "UPDATE UserMaster SET userid = '" + params.email + "' WHERE userid = '" + username + "'";
                         console.log("query: ", sql);
                         executeQuery(sql).then(resolve,reject);  
@@ -57,17 +63,75 @@ exports.handler = async (event, context) => {
                 break;
                 
                 case 'DELETE': 
-                    sql = "DELETE FROM UserMaster where userid = '" + username + "'";
+                    var userPoolData;
+                    var subscriptionData;
+                    var userMasterData;
+                    sql = "SELECT * FROM UserMaster where userid = '" + username + "'";
                     
-                    executeQuery(sql).then(function(){ 
-                        const cognito = new AWS.CognitoIdentityServiceProvider({ region: process.env.REGION });
-                        cognito.adminDeleteUser({
-                            UserPoolId: process.env.COGNITO_POOLID,
-                            Username: 'sample', //replace to username, use sample for testing purposes to avoid recreating tokens.
-                        }).promise().then(function(){
-                            var emailParam = generateGoodbyeParam(username);
-                            sendEmail(emailParam).then(resolve,reject);
-                        });
+                    executeQuery(sql).then(function(data) { 
+                        userMasterData = data[0];
+                        console.log("userMasterData : ", userMasterData);
+                        sql = "SELECT * FROM UserPool where userid = '" + username + "'";
+
+                        executeQuery(sql).then(function(data) { 
+                            userPoolData = data[0];
+                            console.log("userPoolData : ", userPoolData);
+
+                            sql = "SELECT * FROM Subscription where idUserPool = '" + data[0].idUserPool + "'";
+
+                            executeQuery(sql).then(function(data) { 
+                            subscriptionData = data[0];
+                            console.log("subscriptionData : ", subscriptionData);
+
+                            if (subscriptionData == undefined || subscriptionData == null) {
+
+
+                            }
+
+                            if ((userPoolData.type == 'user' && subscriptionData.subscriptionType =='pp1') ||
+                                (userPoolData.type == 'admin' && userMasterData.usertype != 'E')) {
+
+                                sql = "UPDATE Subscription SET subscriptionStatus = 'cancelled' where idUserPool = '" + userPoolData.idUserPool + "'";
+                                    executeQuery(sql).then(function(data) { 
+                                        //resolve(data);
+                                });
+                            }
+                                //Delete all related data when there's nothing tied to any subscription
+                                sql = "SELECT * FROM Subscription where upid = '" + username + "' and subscriptionStatus = 'active'";
+                                executeQuery(sql).then(function(data) {   
+                                    if (data == undefined || data == null) {
+                                        sql = "DELETE FROM UserProduct  where upid = '" + username + "'";
+                                        executeQuery(sql).then(resolve,reject);
+
+                                        sql = "DELETE FROM ProductMaster  where upid = '" + username + "'";
+                                        executeQuery(sql).then(resolve,reject);
+                                    }   
+                                });
+
+                                //Send Email when Notificaiton is enabled
+                                sql = "SELECT * FROM Notification where userid = '" + username + "'";
+                                executeQuery(sql).then(function(data) {   
+                                    console.log("Notification data : ", data[0]);
+                                    if (data == undefined || data == null) {
+                                        resolve(data);
+                                    }
+                                    else if (data.flag == '1') {
+                                        const cognito = new AWS.CognitoIdentityServiceProvider({ region: process.env.REGION });
+                                        cognito.adminDeleteUser({
+                                            UserPoolId: process.env.COGNITO_POOLID,
+                                            Username: 'sample', //replace to username, use sample for testing purposes to avoid recreating tokens.
+                                        }).promise().then(function(){
+                                            var emailParam = generateGoodbyeParam(username);
+                                            sendEmail(emailParam).then(resolve,reject);
+                                        });
+                                    }
+                                });
+
+                                 //Delete from USerMaster
+                                 sql = "DELETE FROM UserMaster where userid = '" + username + "'";
+                                 executeQuery(sql).then(resolve, reject);
+                            });
+                        }); 
                     });  
                 break;
                     
@@ -92,26 +156,30 @@ exports.handler = async (event, context) => {
 
 function executeQuery(sql) {
     return new Promise((resolve, reject) => {
+        console.log("Executing query: ", sql);
         connection.query(sql, function(err, result) {
             if (err) {
-                console.log("Error: " + err);
+                console.log("SQL Error: " + err);
                 reject(err);
             }
+            //console.log("SQL Result: " + result.JSON);
             resolve(result);
         });
     });
 };
 
-function updateUserAttribute(name, value, username, userPoolId){
+function updateUserAttribute(userAttributes, username, userPoolId){
     let cognitoISP = new AWS.CognitoIdentityServiceProvider({ region: process.env.REGION });
     return new Promise((resolve, reject) => {
+        console.log("userAttributes: ", userAttributes);
         let params = {
-            UserAttributes: [
-                {
-                    Name: name,
-                    Value: value 
-                }
-            ],
+            // UserAttributes: [
+            //     {
+            //         Name: name,
+            //         Value: value 
+            //     }
+            // ],
+            UserAttributes: userAttributes,
             UserPoolId: userPoolId,
             Username: username
         };
@@ -124,15 +192,31 @@ function updateUserAttribute(name, value, username, userPoolId){
 function insertUserMaser(params,username){
     return new Promise((resolve, reject) => {
        
-        let sql = "INSERT INTO UserMaster (userid, name, userType, organization, title, lastLogin, createdOn) \
+        let sql = "INSERT INTO UserMaster (userid, name, userStatus, userType, organization, lastLogin, createdOn) \
             VALUES (\
                 '" + username + "',\
                 '" + params.name + "',\
-                '" + params.userType + "',\
+                'NEW',\
+                'NE',\
                 '" + params.organization + "',\
-                '" + params.title + "',\
                 '" + params.lastLogin + "',\
                 '" + params.created + "')";
+
+        executeQuery(sql).then(resolve,reject);
+    });
+};
+
+function insertNotification(username, upid, params){
+    return new Promise((resolve, reject) => {
+       
+        let sql = "INSERT INTO Notification (userid, notificationTypeID, upid, flag, frequency, lastUpdatedDt) \
+            VALUES (\
+                '" + username + "',\
+                '1',\
+                '" + upid + "',\
+                '1',\
+                '" + params.frequency + "',\
+                '" + params.lastUpdatedDt + "')";
 
         executeQuery(sql).then(resolve,reject);
     });
